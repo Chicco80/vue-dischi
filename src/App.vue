7f04cb36cb5cbd2fa458f51289a6ace78746c7c4
Chicco80 <chicco80lisa@gmail.com>
2022-05-06 17:55:07 +0200

    scaffHolding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   <AppHeader/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import "@/assets/styles/general.scss";
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+<header>
+  <nav class="navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24">
+    </a>
+  </div>
+</nav>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+ 
+}
+</script>
+
+
+<style scoped lang="scss">
+@import "@/assets/styles/general.scss";
+
+nav{
+  background-color: $nav-color;
+}
+
+</style>
